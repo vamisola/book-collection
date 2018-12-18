bf34f5decf74527e2f0c0449b3e2e0aa29465cb3
Vina Compson <vv.compson@icloud.com>
2018-12-18 14:31:04 +1100

    add post route and add body-parser

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
+const bodyParser = require("body-parser");
 
+// in order to read HTTP POST data, we have to use "body-parser" 
+// node module.body - parser is a piece of express middleware 
+// that reads a form's input and stores it as a javascript object 
+// accessible through req.body
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 
@@ -28,6 +34,12 @@ app.get("/books", (req, res) => {
         },
     ]
     res.render("books", {books:books});
+});
+
+app.post("/books", (req, res) => {
+    res.send("You hit the post page")
+    //get data from form and add to book array
+    //redirect back to book page
 });
 
 app.listen(PORT, process.env.IP, () => {
