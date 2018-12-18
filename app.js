16d6c1caca74aaf3bf820815d60c19aa11f9a022
Vina Compson <vv.compson@icloud.com>
2018-12-18 16:35:46 +1100

    add post route to /books

--- app.js
+++ app.js
@@ -10,36 +10,47 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+let books = [
+    {
+        author: "Jessica Townsend",
+        bookTitle: "Nevermoor",
+        image: "https://hachette.azureedge.net/books/thumbnails/9780734418074.jpg?v=17&scale=both&width=440"
+    },
+    {
+        author: "Jessica Townsend",
+        bookTitle: "Wundersmith",
+        image: "https://www.slashfilm.com/wp/wp-content/images/Nevermoor.jpg"
+    },
+    {
+        author: "Jessica Townsend",
+        bookTitle: "Wundersmith",
+        image: "https://d3ddkgxe55ca6c.cloudfront.net/assets/t1536311294/a/e1/f2/186446-ml-1766811.jpg"
+    },
+];
 
 app.get("/", (req, res) => {
     res.render("landing");
 });
 
 app.get("/books", (req, res) => {
-    let books = [
-        {
-            author: "Jessica Townsend",
-            bookTitle: "Nevermoor",
-            image: "https://hachette.azureedge.net/books/thumbnails/9780734418074.jpg?v=17&scale=both&width=440"
-        },
-        {
-            author: "Jessica Townsend",
-            bookTitle: "Wundersmith",
-            image: "https://www.slashfilm.com/wp/wp-content/images/Nevermoor.jpg"
-        },
-        {
-            author: "Jessica Townsend",
-            bookTitle: "Wundersmith",
-            image: "https://d3ddkgxe55ca6c.cloudfront.net/assets/t1536311294/a/e1/f2/186446-ml-1766811.jpg"
-        },
-    ]
+    
     res.render("books", {books:books});
 });
 
 app.post("/books", (req, res) => {
-    res.send("You hit the post page")
+    // res.send("You hit the post page")
     //get data from form and add to book array
+    let bookTitle = req.body.bookTitle;
+    let author = req.body.author;
+    let image = req.body.image;
+    let newBook = {
+        bookTitle : bookTitle,
+        author: author,
+        image: image
+    }
+    books.push(newBook);
     //redirect back to book page
+    res.redirect("/books");
 });
 
 app.get("/books/new", (req, res) => {
